154dcd99436ef07d981110a49c49cd2d43586ad6
Brandon Benbow <brandonwbenbow@gmail.com>
2024-01-13 12:32:14 -0600

    Initial Commit

---
+++ src/Service/Server.ts
@@ -0,0 +1,13 @@
+// Imports Database/Session for Self Management, Should try to isolate it from models
+export class Server {
+    static Start() {
+        let s = new Server(); s.Start();
+        return s;
+    }
+
+    constructor() {}
+
+    async Start() {
+
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Server } from "./Service/Server";
+import 'dotenv/config'
+
+// console.log("Process Variables:", process.env);
+Server.Start();
